0f5bfc5313bcc3e32e35e85016e156ad4a8f94af
Guilherme <guilherme.oliveirasantos@hotmail.com>
2022-10-09 17:28:49 -0300

    Criando o carousel

--- LanchesMac/Controllers/HomeController.cs
+++ LanchesMac/Controllers/HomeController.cs
@@ -12,6 +12,10 @@ namespace LanchesMac.Controllers
             return View();
         }
 
+        public IActionResult Demo()
+        {
+            return View();
+        }       
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
